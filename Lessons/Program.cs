b987190dfea6c7de1e67b504193993750f9019e8
yurtsevichK <nicecurls@icloud.com>
2021-01-07 23:31:08 +0300

    Начал учить C#=), до лета запущу прилагу)

---
+++ Lessons/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace Lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string msg = "Тип {0,8} |\t.NET {1,8} |\tРазмер = {2} \t| MIN = {3,30}\t| MAX = {4}";
+
+            Console.Title = "Типы данных в C#";
+            Console.WriteLine(msg);
+        }
+    }
+}
