add6b0b97ddd9d2c267ab851fd3f4abec5f42f77
yurtsevichK <nicecurls@icloud.com>
2021-01-19 18:11:11 +0300

    upgrade homework7

--- Homework7/Program.cs
+++ Homework7/Program.cs
@@ -9,28 +9,35 @@ namespace Homework7
             uint evenNumbersCount = 0;
             uint oddNumbersCount = 0;
 
+            int oddNumbersSum = 0;
+            int evenNumbersSum = 0;
+
             Console.WriteLine("Введите первое число диапазона");
-            int curentvalue = int.Parse(Console.ReadLine());
+            int curentValue = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Введите последнее число диапазона");
             int limit = int.Parse(Console.ReadLine());
 
-            while (curentvalue <= limit)
+            while (curentValue <= limit)
             {
-                if (curentvalue % 2 == 0)
+                if (curentValue % 2 == 0)
                 {
                     evenNumbersCount++;
+                    evenNumbersSum = curentValue + evenNumbersSum;
                 }
                 else
                 {
                     oddNumbersCount++;
+                    oddNumbersSum += curentValue;
                 }
 
-                curentvalue++;
+                curentValue++;
             }
 
             Console.WriteLine("Количество четных чисел:" + evenNumbersCount);
             Console.WriteLine("Количество нечетных чисел:" + oddNumbersCount);
+            Console.WriteLine("Сумма четных чисел:" + evenNumbersSum);
+            Console.WriteLine("Сумма нечетных чисел:" + oddNumbersSum);
             Console.ReadLine();
         }
     }
